346df85e18b45da719c4de55616d911263580975
JohnKlein985 <John@apexinnovations.com>
2020-06-29 22:20:32 -0500

    Removing data attempt to correct

--- recipeJournalAPI.php
+++ recipeJournalAPI.php
@@ -24,7 +24,8 @@ class recipeJournalAPI {
 				$r['ID'] = $row['ID'];
 				$r['Title'] = $recipeData['title'];
 				$r['SubTitle'] = $recipeData['subTitle'];
-				$r['Image'] = "require('../../assets/recipeImages/" . $recipeData['image'] . "')";
+				// $r['Image'] = "require('../../assets/recipeImages/" . $recipeData['image'] . "')";
+				$r['Image'] = $recipeData['image'];
 				$r['Servings'] = $recipeData['servings'];
 				$r['Serves'] = $recipeData['serves'];
 				$r['PrepTime'] = $recipeData['prepTime'];
